ce78f0bcdd24f2bddddf5fc3e4ab5ea25a437d52
Madhur-Parashar <madhur.cs18@gmail.com>
2020-07-02 18:16:02 +0530

    Updates

---
+++ precache-manifest.95ed9e1550ea18d84d785670af571fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0523e99a3f2a967fb59101841ef0799a",
+    "url": "/Weather-Forecast/index.html"
+  },
+  {
+    "revision": "79b79cd071183b895f54",
+    "url": "/Weather-Forecast/static/css/main.99ff2c53.chunk.css"
+  },
+  {
+    "revision": "fc563ea9dde3c3a2a41b",
+    "url": "/Weather-Forecast/static/js/2.e14d5f90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-Forecast/static/js/2.e14d5f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b79cd071183b895f54",
+    "url": "/Weather-Forecast/static/js/main.69e255a3.chunk.js"
+  },
+  {
+    "revision": "5955f1aad560f565e8a1",
+    "url": "/Weather-Forecast/static/js/runtime-main.a3c7ca31.js"
+  }
+]);
